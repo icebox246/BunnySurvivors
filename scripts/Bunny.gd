0f6de3a5368c20d347d568287dcba5930ef57b0c
icebox246 <iceboxpoczta@gmail.com>
2023-06-02 12:18:58 +0200

    Nit

--- scripts/Bunny.gd
+++ scripts/Bunny.gd
@@ -2,18 +2,29 @@ class_name Bunny extends CharacterBody2D
 
 const SPEED = 200
 @onready var game_master = $"/root/GameMaster"
+@onready var attack_timer = $AttackTimer
 
 func _ready():
 	$AnimatedSprite2D.play()
 
 func _physics_process(_delta):
-	var to_player = (game_master.player_position() - position).normalized()
-	velocity = to_player * SPEED
+	var to_player = game_master.player_position() - position
+	var distance = to_player.length()
+	to_player = to_player.normalized()
 
-	if velocity.x:
-		$AnimatedSprite2D.flip_h = velocity.x > 0
+	if distance > 60:
+		velocity = to_player * SPEED
+		if velocity.x:
+			$AnimatedSprite2D.flip_h = velocity.x > 0
+	elif attack_timer.is_paused():
+		attack_timer.start()
 
 	move_and_slide()
 
 func bonk():
+	game_master.add_score(100)
 	queue_free()
+
+func _on_attack_timer_timeout():
+	print("Gnaw")
+
--- scripts/GameMaster.gd
+++ scripts/GameMaster.gd
@@ -1,9 +1,12 @@
 extends Node
 
+var score = 0
+
 var player_node = null
 
 func register_player(player: Node2D):
 	print("Registerd player: ", player)
+	score = 0
 	player_node = player
 
 func player_position():
@@ -12,17 +15,34 @@ func player_position():
 	else:
 		return Vector2(0,0)
 
-const SPAWN_INTERVAL = 5
+var score_label_node = null
+
+func register_score_label(score_label: Label):
+	score_label_node = score_label
+	_update_score_label()
+
+func add_score(amount: int):
+	score += amount
+	_update_score_label()
+
+func _update_score_label():
+	if score_label_node:
+		score_label_node.text = "Score: " + str(score)
+
+const SPAWN_INTERVAL = 3
 const SPAWN_RADIUS = 500
-var time_since_last_spawn = 0
+var time_since_last_spawn = 4
 
 var bunny_prefab = preload("res://prefabs/Bunny.tscn")
 
 func _process(delta):
 	time_since_last_spawn += delta
 
-	if time_since_last_spawn >= SPAWN_INTERVAL:
-		time_since_last_spawn -= SPAWN_INTERVAL
-		var bunny = bunny_prefab.instantiate()
-		bunny.position = player_position() + Vector2.from_angle(randf() * PI * 2) * SPAWN_RADIUS
-		get_tree().current_scene.add_child(bunny)
+	if player_node:
+		if time_since_last_spawn >= SPAWN_INTERVAL:
+			time_since_last_spawn -= SPAWN_INTERVAL
+			var bunny = bunny_prefab.instantiate()
+			bunny.position = player_position() + Vector2.from_angle(randf() * PI * 2) * SPAWN_RADIUS
+			get_tree().current_scene.add_child(bunny)
+
+
